cbb132dd9e10026a769bf2d6427bc1a82ebf84af
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-03-13 23:06:09 +0100

    Integrate Canvas, Home and Customizer into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-      <h1 className="head-text">ThreeJS</h1>
-    </>
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   );
 }
 
