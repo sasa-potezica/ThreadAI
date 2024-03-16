046d386c08f04536e9127f0dd4ea3edaa39b499e
Sasa Potezica <sasapotezica.contact@gmail.com>
2024-03-16 23:09:37 +0100

    Integrate FilePicker and AIPicker components into Customizer

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -36,9 +36,26 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={handlePrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+    try {
+    } catch (error) {
+      allert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
   const handleDecals = (type, result) => {
@@ -59,9 +76,13 @@ const Customizer = () => {
       default:
         state.isLogoTexture = true;
         state.isFullTexture = false;
-
-        break;
     }
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      };
+    });
   };
   const readFile = (type) => {
     reader(file).then((result) => {
@@ -114,8 +135,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
